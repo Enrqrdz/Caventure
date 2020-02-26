e44252d2ba8a15382274336b6101b4e681645a19
SamuelBolano <samuelbolano1996@gmail.com>
2020-02-25 19:31:59 -0600

    wall and floor Scripts

---
+++ Caventure/Assets/Art/LPGeometry/models_new/Scripts/EnvironmentAnimationScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironemtnAnimationScript : MonoBehaviour
+{
+    public int playTime;
+    bool playing;
+    Animator anim; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        anim = GetComponent<Animator>();
+
+    }
+  
+
+   public void PlayAnim()
+    {
+        anim.SetTrigger("Break");
+
+        Destroy(this.gameObject,playTime);
+
+
+    }
+}
