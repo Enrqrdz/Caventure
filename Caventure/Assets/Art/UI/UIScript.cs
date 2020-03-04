e4ef254062f888cf92738da690ce2f037d2fe65b
SamuelBolano <samuelbolano1996@gmail.com>
2020-03-03 23:09:35 -0600

    UI stuff
    animated UI elements

---
+++ Caventure/Assets/Art/UI/UIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public float speed;
+    float timer;
+    float sin1;
+    public float size1 = 3f;
+    public float size2 = 2f;
+    public float size3 = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += speed*Time.deltaTime;
+
+        sin1= Mathf.Sin(timer)/size3;
+        
+        
+        
+        
+        transform.localScale= new Vector3(sin1+size1/size2, sin1 + size1 / size2, sin1 + size1 / size2);
+    }
+}
